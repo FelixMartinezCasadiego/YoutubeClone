d11af4b0b512a90768c1b09289ac54e8f310099c
Felix Martinez Casadiego <felix.martinezcasadiego@gmail.com>
2021-10-10 22:39:41 -0300

    Se realiza parte responsive de la página

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,22 @@
+// Animación para barra lateral
 const contenedor = document.querySelector('#contenedor')
 
 document.querySelector('#botonMenu').addEventListener('click', () => {
     contenedor.classList.toggle('active');
 } )
+
+// Parte responsive
+
+const comprobarAncho = () => {
+    if(window.innerWidth <= 768){
+        contenedor.classList.remove('active');
+    } else {
+        contenedor.classList.add('active');
+    }
+}
+
+comprobarAncho() ;
+
+window.addEventListener ('resize', () => {
+    comprobarAncho () ;
+} );
